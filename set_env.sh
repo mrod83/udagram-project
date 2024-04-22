bf6ddaf1b7eed2a6b77a28bada7cdcb757d903fe
Marel Oliva <moliva@devsu.com>
2024-04-22 11:06:43 -0600

    Implement k8

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,12 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_PASSWORD=12345678
+export POSTGRES_HOST=mjorod.cb2088ua2k0s.us-east-2.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
-export AWS_REGION=us-east-1
+export AWS_BUCKET=test-590183762636-dev
+export AWS_REGION=us-east-2
 export AWS_PROFILE=default
-export JWT_SECRET=testing
+export JWT_SECRET=hello
 export URL=http://localhost:8100
+export PORT=8080
--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a3faf6d5ff5c4492d973cc311cbf8e20-1162351995.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
